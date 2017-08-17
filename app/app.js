7a0aab2c491bcb29ed1d74dc79635d2d2846693c
Rochelle Beal <bealrochelle@gmail.com>
2017-08-17 16:47:32 -0500

    Changed React Components

--- app/app.js
+++ app/app.js
@@ -1,9 +1,23 @@
-// Include the Main React Dependencies
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+// Include the react-router module
+// Include the Route component for displaying individual routes
+// Include the Router component to contain all our Routes
+// Include the hashHistory prop to handle routing client side without a server
+// Include the IndexRoute (catch-all route)
+import { BrowserRouter, Route } from 'react-router-dom'
 
-import Main from "./components/Main";
+// Reference the high-level components
+import { Navbar } from "./components/Navbar";
+import { Landing } from "./components/Landing";
 
-
-// This code here allows us to render our main component (in this case "Parent")
-ReactDOM.render(<Main />, document.getElementById("app"));
+const app =  document.getElementById("app");
+ReactDOM.render (
+    <BrowserRouter>
+        <div className="container">
+            <Navbar />
+            <Route exact path="/" component={ Landing } />
+        </div>
+    </BrowserRouter>, 
+    app
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     // This is the entry point or start of our react applicaton
-    entry: "./routes/index.js",
+    entry: "./app/app.js",
 
     // The plain compiled JavaScript will be output into this file
     output: {
