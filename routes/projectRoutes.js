d759d56a1961c843ce7ac3b9973a39c648ef4e77
Rochelle Beal <bealrochelle@gmail.com>
2017-08-26 08:55:15 -0500

    Heroku deployment changes

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -3,7 +3,7 @@ const Project = require("../models/Project");
 
 // This is the route we will send GET requests to retrieve our project data.
 module.exports = app => {
-    app.get('/api/projects', (req, res,) => {
+    app.get('/api/projects', (req, res) => {
         Project.getProjects((err, doc) => {   
             if (err) {
                 console.log(err);
