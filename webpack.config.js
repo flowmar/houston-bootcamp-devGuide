5ae7aa8812a144dfdabb2e0def50c72e484eb3e6
Rochelle Beal <bealrochelle@gmail.com>
2017-08-26 01:36:32 -0500

    Heroku Deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     // This is the entry point or start of our react applicaton
-    entry: ["babel-polyfill", __dirname + "/app/index.js"],
+    entry: "./app/index.js",
 
     // The plain compiled JavaScript will be output into this file
     output: {
