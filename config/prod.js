f5bc86cdbacc13ca0b9fce4185b238f0d44970ec
Rochelle Beal <bealrochelle@gmail.com>
2017-08-26 00:40:03 -0500

    Heroku Deployment

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,6 @@ module.exports = {
     googleClientID: process.env.GOOGLE_CLIENT_ID,
     googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
     cookieKey: process.env.COOKIE_KEY,
-    mongoURI: process.env.MONGO_URI
+    mongoURI: process.env.MONGODB_URI
 };
 
