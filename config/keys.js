44071aca4e360176b9b3dc928a4ce605c161270e
Flowmar <omar.imam@kineticvortex.com>
2017-08-26 23:25:13 -0500

    add to .gitignore

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,7 @@
 if (process.env.NODE_ENV === 'production') {
     // we are in production - return the prod set of keys
-     module.exports = require('./prod');
-
-}else{
+    module.exports = require('./prod');
+} else {
     // we are in development - return the dev keys
-    module.exports = require('./dev');
+    module.exports = require('./dev/keys.js');
 }
--- config/passport.js
+++ config/passport.js
@@ -1,46 +1,46 @@
 // =============== Google OAuth Login ================//
-const passport = require("passport");
+const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 // call User model to load something into it
 const User = mongoose.model('users');
 const keys = require('./keys');
-// Call serializeUser with the user to generate the 
-// identifying piece of information to then be used
-// by passport to set-up a cookie.
-// Use user.id that is assigned to the user by mongo "_id"
+// Call serializeUser with the user to generate the identifying piece of
+// information to then be used by passport to set-up a cookie. Use user.id that
+// is assigned to the user by mongo "_id"
 passport.serializeUser((user, done) => {
     done(null, user.id);
 });
 
-// We will take the user.id which we stuck in a cookie and turn it
-// back into an actual User model.
+// We will take the user.id which we stuck in a cookie and turn it back into an
+// actual User model.
 passport.deserializeUser((id, done) => {
-    User.findById(id).then(user => {
-        done(null, user);
-    })
+    User
+        .findById(id)
+        .then(user => {
+            done(null, user);
+        });
 });
 
-//Create new instance of Google Strategy
-//Create a new user if one doesn't already exist.
-passport.use(new GoogleStrategy (
-    {
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: 'https://devguide2017.herokuapp.com/auth/google/callback',
-        proxy: true
-    }, 
-    (accessToken, refreshToken, profile, done) => {
-        //console.log("profile", profile);
-        User.findOne({googleId: profile.id}).then(existingUser => {
+// Create new instance of Google Strategy Create a new user if one doesn't
+// already exist.
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+    proxy: true
+}, (accessToken, refreshToken, profile, done) => {
+    //console.log("profile", profile);
+    User
+        .findOne({googleId: profile.id})
+        .then(existingUser => {
             // we already have a record in the database
-            if(existingUser) {
+            if (existingUser) {
                 done(null, existingUser);
             } else {
-            new User ({ googleId: profile.id})
-            .save()
-            .then(user => done(null, user));
+                new User({googleId: profile.id})
+                    .save()
+                    .then(user => done(null, user));
             }
-        })
-    })
-);
+        });
+}));
